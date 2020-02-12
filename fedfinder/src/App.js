2ab8bdefdac11a93b50b4fb6b0fbcb925c33fcb3
nicholasccrockett <nicholasccrockett@gmail.com>
2020-02-12 09:48:16 -0600

    Modified Css so that it looks a bit more uniform and should change with resizing of windows.

--- fedfinder/src/App.js
+++ fedfinder/src/App.js
@@ -1,4 +1,4 @@
-import React, {Component} from 'react';
+import React from 'react';
 import logo from './assets/FedReserveLogo.svg';
 import floor from './assets/floor1.svg';
 import Info from './components/Info';
--- fedfinder/src/components/Info.js
+++ fedfinder/src/components/Info.js
@@ -1,4 +1,4 @@
-import React, {Component} from 'react';
+import React from 'react';
 import {Tab, Tabs, TabList, TabPanel} from 'react-tabs';
 import {style} from 'react-tabs';
 import './Comp.css';
