67a96ea188fdf12f27d6bd65210c85f3189c76ba
nicholasccrockett <nicholasccrockett@gmail.com>
2020-02-11 20:29:45 -0600

    Added React-tabs (you might have to use [npm install react-tabs]) and added 4 tabs for the Info-Hub including Home, Descriptor, Search, and Help. All this is mostly placeholders but its there at least

--- fedfinder/src/components/Info.js
+++ fedfinder/src/components/Info.js
@@ -1,10 +1,11 @@
 import React, {Component} from 'react';
+import {Tab, Tabs, TabList, TabPanel} from 'react-tabs';
+import {style} from 'react-tabs';
 import './Comp.css';
  
 class Info extends React.Component {
     state = {
-        page: 'Information Hub',
-        body: 'This is the information hub'
+        page: 'Information Hub'
     }
 
     render () {
@@ -12,10 +13,31 @@ class Info extends React.Component {
 
         return (
             <div className="Info-Hub">
-                <h1> {this.state.page} </h1>
-                <p> {this.state.body} </p>
                 <div className="Hub">
-                    <p>{this.props.activehub}</p>
+                    <Tabs>
+                        <TabList className="Hub-Nav">
+                            <Tab className="Tab">Home</Tab>
+                            <Tab className="Tab">Descriptor</Tab>
+                            <Tab className="Tab">Search</Tab>
+                            <Tab className="Tab">Help</Tab>
+                        </TabList>
+
+                        <TabPanel className="TabPanel">
+                            <div>
+                            <h1> Welcome to the FedFinder </h1>
+                            <p> This box here is the Information Hub. </p>
+                            </div>
+                        </TabPanel>
+                        <TabPanel className="TabPanel">
+                            <div>Descriptor</div>
+                        </TabPanel>
+                        <TabPanel className="TabPanel">
+                            <div>Search</div>
+                        </TabPanel>
+                        <TabPanel className="TabPanel">
+                            <div>Help</div>
+                        </TabPanel>
+                    </Tabs>
                 </div>
             </div>
         );
